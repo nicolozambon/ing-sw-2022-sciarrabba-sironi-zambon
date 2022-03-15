1b283713246cd921318df274312118fcb4a29568
AlessandroSironi <alessandro1.sironi@mail.polimi.it>
2022-03-15 17:30:56 +0100

    Added methods in Deck and AssistantCard

--- src/main/java/it/polimi/ingsw/model/AssistantCard.java
+++ src/main/java/it/polimi/ingsw/model/AssistantCard.java
@@ -3,4 +3,12 @@ package it.polimi.ingsw.model;
 public class AssistantCard extends Card {
     private int value;
     private int movement;
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getMovement() {
+        return movement;
+    }
 }
--- src/main/java/it/polimi/ingsw/model/Deck.java
+++ src/main/java/it/polimi/ingsw/model/Deck.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class Deck {
     private ArrayList<Card> cardArrayList;
@@ -14,5 +16,24 @@ public class Deck {
      **/
     public Card extractCard() {
         return null;
+    }
+
+    /**
+     * Shuffles the given deck.
+     * @param deck to shuffle.
+     */
+    public void shuffleDeck(Deck deck) {
+        Collections.shuffle(deck.cardArrayList);
+    }
+
+    /**
+     * @param deck1 departure
+     * @param deck2 destination
+     * @param card to be moved
+     */
+    public void moveCard(Deck deck1, Deck deck2, Card card) {
+        int position = deck1.cardArrayList.indexOf(card);
+        deck2.cardArrayList.add(deck1.cardArrayList.get(position));
+        deck1.cardArrayList.remove(card);
     }
 }
