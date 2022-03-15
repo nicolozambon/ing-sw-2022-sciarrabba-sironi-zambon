8e8c05175808ddc3eec85c497556d9469ff960ad
ZetaEnne <35655901+zetaenne@users.noreply.github.com>
2022-03-15 13:50:25 +0100

    Delete initial_uml.png and add plantuml jar

---
+++ src/main/java/it/polimi/ingsw/model/NoEntryTile.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.model;
+
+public class NoEntryTile {
+    Island island;
+
+    public NoEntryTile(Island island) {
+        this.island = island;
+    }
+
+    /**
+     *
+     * @return the island on which the blocktile is on.
+     */
+    public Island getIsland() {
+        return island;
+    }
+}
