ad7beb956af44823bb8bced6ecd8fae501bd71cb
AlessandroSironi <alessandro1.sironi@mail.polimi.it>
2022-03-21 15:20:28 +0100

    Minor changes in Round

--- src/main/java/it/polimi/ingsw/model/Round.java
+++ src/main/java/it/polimi/ingsw/model/Round.java
@@ -1,37 +1,49 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.card.AssistantCard;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Round (List<Player> playerOrder, StudentBag sb, MotherNature mn) {
+public class Round {
 
     private StudentBag studentbag;
     private MotherNature mothernature;
 
-    public Round () {
+    private List<Player> playerOrder;
+
+    public Round (List<Player> playerOrder, StudentBag sb, MotherNature mn) {
         this.studentbag = sb;
         this.mothernature = mn;
+        this.playerOrder = playerOrder;
     }
 
+    /**
+     * Planning phase of the game.
+     * Step 1 -> Populate all 2(3) clouds with 3 students.
+     * Step 2 -> Choose an assistant card and put it in the discard deck.
+     * @param clouds All Clouds.
+     */
     public void planningPhase (Cloud ... clouds) {
+        ArrayList<AssistantCard> playedCardInRound;
         //Step 1
         for(Cloud cloud : clouds) {
             studentbag.extractStudentAndMove(cloud);
         }
 
         //Step 2
-        for (Player : playerOrder) {
-            //TODO: choose an assistant card and put it in the discard deck.
-            //TODO: check that the choice can be made (one cannot choose to play the same assistant card already played by another player in the same round.
-                    // In the rare case you only have a matching card, you must play it. However, if you do so, you will play after the player who played the card first.
+        for (Player p : playerOrder) {
+            //choice = askuser; //TODO: Implement a way to get the choice of the user.
+            playedCardInRound.add(choice);
+            if (choice != ) {
+                    p.playAssistantCard(choice)
+            };
         }
     }
 
     public void actionPhase () {
-        for (Player : playerOrder) {
-            for (int i = 0; i < 3; i++) {//TODO: game with 3 players?
+        for (Player p : playerOrder) {
 
-            }
         }
     }
 
