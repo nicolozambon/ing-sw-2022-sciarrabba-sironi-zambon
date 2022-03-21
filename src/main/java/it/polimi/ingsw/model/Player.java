4ba21bcf4448aeb9b0a7e92dda7b4fa0d447d246
AlessandroSironi <alessandro1.sironi@mail.polimi.it>
2022-03-21 14:07:26 +0100

    Minor changes in Player

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -9,13 +9,15 @@ public class Player {
 
     private String nickname;
     public final School school;
+    public final int id;
 
     private Deck AssistantCardDeck;
     private Deck DiscardPileDeck;
 
 
-    public Player(String nickname, List<Student> students, List<Tower> towers) {
+    public Player(String nickname, List<Student> students, List<Tower> towers, int id) {
         this.nickname = nickname;
+        this.id = id;
         school = new School(this, students, towers);
         this.AssistantCardDeck = new DeckAssistantCard(null);
         this.DiscardPileDeck = new Deck (null);
@@ -28,4 +30,5 @@ public class Player {
     public String getNickname() {
         return this.nickname;
     }
+
 }
