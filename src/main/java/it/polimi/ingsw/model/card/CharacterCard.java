ef7ac571a1a369e5cebf0c60fd5212eb67950013
ZetaEnne <35655901+zetaenne@users.noreply.github.com>
2022-03-21 15:58:54 +0100

    Add overrides for CharacterCard

--- src/main/java/it/polimi/ingsw/model/card/CharacterCard.java
+++ src/main/java/it/polimi/ingsw/model/card/CharacterCard.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.model.card;
 
+import java.util.Objects;
+
 public class CharacterCard extends Card {
 
     private final int id;
@@ -18,4 +20,16 @@ public class CharacterCard extends Card {
         this.effect = effect;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CharacterCard)) return false;
+        CharacterCard that = (CharacterCard) o;
+        return coins == that.coins && Objects.equals(setup, that.setup) && Objects.equals(effect, that.effect);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(coins, setup, effect);
+    }
 }
