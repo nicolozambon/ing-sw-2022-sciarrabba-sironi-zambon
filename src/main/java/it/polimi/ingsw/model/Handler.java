dfb1bfe0b8a9832e330a460556ab423c52cbbb4f
AlessandroSironi <alessandro1.sironi@mail.polimi.it>
2022-04-04 15:31:32 +0200

    Default handler - changes by all

--- src/main/java/it/polimi/ingsw/model/Handler.java
+++ src/main/java/it/polimi/ingsw/model/Handler.java
@@ -13,7 +13,6 @@ public class Handler {
         this.players = players;
     }
 
-    //TODO this method works but not much scalable for character card effects
     protected void professorControl(Player currentPlayer, Color color, Board<Professor> startingProfBoard) {
         Professor professor = getProfessor(color, startingProfBoard);
         Board<Professor> professorBoard = getProfessorBoard(color, startingProfBoard);
@@ -40,17 +39,84 @@ public class Handler {
     protected void motherNatureMovement(Player currentPlayer, MotherNature motherNature, int stepsChoice) {
         if(stepsChoice > 0 && stepsChoice <= currentPlayer.getLastAssistantCard().getSteps()) {
             motherNature.stepsToMove(stepsChoice);
-            resolveIsland(motherNature.getPosition());
+            switchTowers(motherNature.getPosition(), getMostInfluentialPlayer(motherNature.getPosition()));
             unifyIsland(motherNature.getPosition());
         }
     }
 
-    protected void resolveIsland(Island island) {
+    protected int resolveIsland(Island island, Player player) {
+        int influence = 0;
 
+        while (island.isUnifyPrev()) {
+            island = island.getPrevIsland();
+        }
+
+        influence += resolveIslandHelper(island, player, influence);
+
+        while (island.isUnifyNext()) {
+            island = island.getNextIsland();
+            influence += resolveIslandHelper(island, player, influence);
+        }
+        return influence;
     }
 
-    protected void getMostInfluentialPlayer(Island island) {
+    protected int resolveIslandHelper (Island island, Player player, int influence) {
+        for (Professor professor : player.getSchool().getProfessorsTable().getPawns()) {
+            influence += island.countStudentsByColor(professor.getColor());
+        }
 
+        if (island.getTower().getOwner().equals(player)) {
+            influence += 1;
+        }
+        return influence;
+    }
+
+    /**
+     * Calculates for every player the influence on each (group of) island.
+     * @param island
+     * @return the most influential player
+     */
+    protected Player getMostInfluentialPlayer(Island island) {
+        Map<Player, Integer> playerInfluence = new HashMap<Player, Integer>();
+        int maxInfluence = -1;
+        Player playerMaxInfluence = null;
+        boolean isValid = false;
+
+        for (Player player : players) {
+            playerInfluence.put(player, resolveIsland(island, player));
+        }
+
+        for (Player player : players) {
+            if (playerInfluence.get(player) > maxInfluence) {
+                maxInfluence = playerInfluence.get(player);
+                playerMaxInfluence = player;
+                isValid = true;
+            } else if (playerInfluence.get(player) == maxInfluence) {
+                isValid = false;
+            }
+        }
+
+        if (isValid) return playerMaxInfluence;
+
+        return null;
+    }
+
+    /**
+     * Switches the ownership of all island linked together, changing the towers.
+     * @param island
+     * @param newOwner
+     */
+    private void switchTowers (Island island, Player newOwner) {
+        while (island.isUnifyPrev()) {
+            island = island.getPrevIsland();
+        }
+
+        island.setTower(newOwner);
+
+        while (island.isUnifyNext()) {
+            island = island.getNextIsland();
+            island.setTower(newOwner);
+        }
     }
 
     private void unifyIsland(Island island) {
@@ -74,11 +140,11 @@ public class Handler {
                 island.getPrevIsland().unifyToNext();
                 unifyIsland(island.getPrevIsland());
             }
-
         }
     }
 
     protected void extraAction(int value, Model model) {
+        // @Override
     }
 
 
