5c21e49ded5a6dba3e5e3202fe89a350136e09cb
ZetaEnne <35655901+zetaenne@users.noreply.github.com>
2022-04-05 10:31:13 +0200

    Tests on AssistantCard and CharacterCard

--- src/test/java/it/polimi/ingsw/model/card/AssistantCardTest.java
+++ src/test/java/it/polimi/ingsw/model/card/AssistantCardTest.java
@@ -1,16 +1,44 @@
 package it.polimi.ingsw.model.card;
 
+import com.google.gson.Gson;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AssistantCardTest {
+    private AssistantCard[] tests;
 
-    @Test
-    void getValue() {
+    public AssistantCardTest(AssistantCard test) {
+        try {
+            Gson gson = new Gson();
+            this.tests = gson.fromJson(new FileReader("src/main/resources/json/assistant_cards.json"), AssistantCard[].class);
+        } catch (FileNotFoundException exception) {}
     }
 
     @Test
-    void getSteps() {
+    void lengthNotNull() {
+        assertNotEquals(0, this.tests.length);
+    }
+
+    @Test
+    void valueBetweenOneAndTen() {
+        int value = 0;
+        for (AssistantCard card : this.tests) {
+            value = card.getValue();
+            assertTrue(value <= 10 && value >= 1);
+        }
+    }
+
+    @Test
+    void stepsBetweenOneAndFive() {
+        int steps = 0;
+        for (AssistantCard card : this.tests) {
+            steps = card.getSteps();
+            assertTrue(steps <= 5 && steps >= 1);
+        }
     }
 }
--- src/test/java/it/polimi/ingsw/model/card/CharacterCardTest.java
+++ src/test/java/it/polimi/ingsw/model/card/CharacterCardTest.java
@@ -1,7 +1,36 @@
 package it.polimi.ingsw.model.card;
 
+import com.google.gson.Gson;
+import it.polimi.ingsw.model.ModelBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CharacterCardTest {
+    private CharacterCard[] tests;
 
+    public CharacterCardTest(CharacterCard test) {
+        try {
+            Gson gson = new Gson();
+            this.tests = gson.fromJson(new FileReader("src/main/resources/json/character_cards.json"), CharacterCard[].class);
+        } catch (FileNotFoundException exception) {}
+    }
+
+    @Test
+    void lengthNotNull() {
+        assertNotEquals(0, this.tests.length);
+    }
+
+    @Test
+    void coinsBetweenOneAndThree() {
+        int coins = 0;
+        for (CharacterCard card : this.tests) {
+            coins = card.getCoins();
+            assertTrue(coins <= 3 && coins >= 1);
+        }
+    }
 }
