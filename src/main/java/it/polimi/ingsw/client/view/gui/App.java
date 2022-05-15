31d66eba3c2d165e477f3083e5f4351f44675105
AlessandroSironi <alessandro1.sironi@mail.polimi.it>
2022-05-15 15:27:43 +0200

    GUI

--- src/main/java/it/polimi/ingsw/client/view/gui/App.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/App.java
@@ -1,14 +1,20 @@
 package it.polimi.ingsw.client.view.gui;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class App extends Application {
     public static void main(String[] args) {
@@ -16,7 +22,8 @@ public class App extends Application {
     }
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
+        /*
         stage.setTitle("Hello World!");
         Button btn = new Button();
         Label label = new Label();
@@ -33,6 +40,25 @@ public class App extends Application {
         StackPane stackPane = new StackPane(btn, label);
         StackPane.setAlignment(label, Pos.BOTTOM_CENTER);
         stage.setScene(new Scene(stackPane, 300, 250));
+        stage.show(); */
+
+        Parent root = FXMLLoader.load(getClass().getResource("/assets/gui/scenes/startMenu.fxml"));
+
+        Scene scene = new Scene(root, 980, 720);
+
+        RadioButton rBtnIP = (RadioButton) scene.lookup("#customIPID");
+        TextField textFieldIP = (TextField) scene.lookup("#textFieldIP");
+        rBtnIP.setOnAction(actionEvent -> {
+            if (rBtnIP.isSelected()) {
+                textFieldIP.setStyle("display: block");
+            } else {
+                textFieldIP.setStyle("display: none");
+            }
+        });
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
         stage.show();
+
+
     }
 }
