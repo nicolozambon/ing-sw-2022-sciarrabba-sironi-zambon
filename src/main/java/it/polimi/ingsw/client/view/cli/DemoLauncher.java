dcdfa27f0a4532c6ea2ffc1463de14329f3f516a
ZetaEnne <35655901+zetaenne@users.noreply.github.com>
2022-05-17 15:20:33 +0200

    CLI error on Windows solved

--- src/main/java/it/polimi/ingsw/client/view/cli/DemoLauncher.java
+++ src/main/java/it/polimi/ingsw/client/view/cli/DemoLauncher.java
@@ -12,34 +12,70 @@ public class DemoLauncher {
 
     public static void main(String[] args) throws IOException {
 
-        final AssetsLoader loader = new AssetsLoader();
-
-        String[][] school = loader.getSchool(false);
-        System.out.println(school.length);
-        System.out.println(school[0].length);
-
-        String[][] island = loader.getIsland();
-        System.out.println(island.length);
-        System.out.println(island[0].length);
-
-        String[][] cloud = loader.getIsland();
-        System.out.println(cloud.length);
-        System.out.println(cloud[0].length);
-
-        String[][] character_card = loader.getIsland();
-        System.out.println(character_card.length);
-        System.out.println(character_card[0].length);
-
-        /*
-
         CLI cli = new CLI(3);
 
+        cli.addStudentToSchoolDiningRoom(0, Color.RED);
+        cli.addStudentToSchoolDiningRoom(0, Color.BLUE);
+        cli.addProfessorToSchool(0, Color.BLUE);
+        cli.addStudentToCloud(0, Color.BLUE);
 
+
+        cli.addStudentToSchoolDiningRoom(1, Color.BLUE);
+        cli.addStudentToSchoolDiningRoom(1, Color.BLUE);
+        cli.addStudentToSchoolDiningRoom(1, Color.BLUE);
+        cli.addStudentToSchoolDiningRoom(1, Color.BLUE);
+        cli.addStudentToSchoolDiningRoom(1, Color.BLUE);
+        cli.addStudentToSchoolDiningRoom(1, Color.BLUE);
+        cli.addStudentToSchoolDiningRoom(1, Color.BLUE);
+        cli.addStudentToSchoolDiningRoom(1, Color.BLUE);
+        cli.addStudentToSchoolDiningRoom(1, Color.BLUE);
+
+        cli.addStudentToSchoolDiningRoom(1, Color.YELLOW);
+        cli.addStudentToSchoolDiningRoom(1, Color.YELLOW);
+        cli.addStudentToSchoolDiningRoom(1, Color.YELLOW);
+        cli.addStudentToSchoolDiningRoom(1, Color.YELLOW);
+
+        cli.addStudentToSchoolDiningRoom(1, Color.RED);
+        cli.addStudentToSchoolDiningRoom(1, Color.RED);
+        cli.addStudentToSchoolDiningRoom(1, Color.RED);
+        cli.addStudentToSchoolDiningRoom(1, Color.RED);
+        cli.addStudentToSchoolDiningRoom(1, Color.RED);
+        cli.addStudentToSchoolDiningRoom(1, Color.RED);
+        cli.addStudentToSchoolDiningRoom(1, Color.RED);
+        cli.addStudentToSchoolDiningRoom(1, Color.RED);
+
+        cli.addStudentToSchoolDiningRoom(1, Color.PINK);
+        cli.addStudentToSchoolDiningRoom(1, Color.PINK);
+        cli.addStudentToSchoolDiningRoom(1, Color.PINK);
+        cli.addStudentToSchoolDiningRoom(1, Color.PINK);
+        cli.addStudentToSchoolDiningRoom(1, Color.PINK);
+
+        cli.addStudentToSchoolDiningRoom(1, Color.GREEN);
+        cli.addStudentToSchoolDiningRoom(1, Color.GREEN);
+        cli.addStudentToSchoolDiningRoom(1, Color.GREEN);
+        cli.addStudentToSchoolDiningRoom(1, Color.GREEN);
+        cli.addStudentToSchoolDiningRoom(1, Color.GREEN);
+        cli.addStudentToSchoolDiningRoom(1, Color.GREEN);
+        cli.addStudentToSchoolDiningRoom(1, Color.GREEN);
+
+
+
+
+        cli.addLinkToNextIsland(0);
+        cli.addLinkToNextIsland(1);
+        cli.addLinkToNextIsland(2);
+        cli.addLinkToNextIsland(3);
+        cli.addLinkToNextIsland(4);
+        cli.addLinkToNextIsland(5);
+        cli.addLinkToNextIsland(6);
+        cli.addLinkToNextIsland(7);
+        cli.addLinkToNextIsland(8);
+        cli.addLinkToNextIsland(9);
+        cli.addLinkToNextIsland(10);
+        cli.addLinkToNextIsland(11);
 
         // Show updated game board
         cli.showGameBoard();
-
-         */
     }
 
 }
