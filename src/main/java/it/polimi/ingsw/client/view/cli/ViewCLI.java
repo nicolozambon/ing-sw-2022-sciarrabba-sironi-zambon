c48ce277b792f747527086dd0cd7af7356fc4749
Jonathan Sciarrabba <jonathan.sciarrabba@mail.polimi.it>
2022-05-29 01:54:23 +0200

    small fix

--- src/main/java/it/polimi/ingsw/client/view/cli/ViewCLI.java
+++ src/main/java/it/polimi/ingsw/client/view/cli/ViewCLI.java
@@ -50,7 +50,7 @@ public class ViewCLI implements AnswerListener, RequestListenableInterface {
             case "options" -> handleOptions(answerEvent);
             case "update" -> updateModel(answerEvent);
             case "wait" -> {
-                if (this.model != null) cliBuilder.buildCLI(model, nickname);
+                if (this.model != null) cliBuilder.buildCLI(model, nickname).showGameBoard();
                 System.out.print(optionLister.list(answerEvent.getPropertyName()));
                 if (answerEvent.getMessage() != null) System.out.print(", it is " + answerEvent.getMessage() + "'s turn");
                 System.out.print("\n");
