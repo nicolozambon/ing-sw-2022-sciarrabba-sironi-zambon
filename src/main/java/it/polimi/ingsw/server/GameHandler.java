b6784c28552cdeb220b37f7dc13f855e3d5abc85
ZetaEnne <35655901+zetaenne@users.noreply.github.com>
2022-06-24 15:39:02 +0200

    CLI refactor

--- src/main/java/it/polimi/ingsw/server/GameHandler.java
+++ src/main/java/it/polimi/ingsw/server/GameHandler.java
@@ -11,7 +11,10 @@ import it.polimi.ingsw.model.Model;
 import it.polimi.ingsw.model.ModelBuilder;
 import it.polimi.ingsw.model.Player;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,23 +27,44 @@ public class GameHandler implements Runnable {
     private final Model model;
     private final Controller controller;
     private final Map<Integer, ConnectionHandler> playersConnection;
+    private final String saveName;
+    private final Gson gson;
 
     public GameHandler(Map<String, ConnectionHandler> playersConnection, boolean completeRule) {
+        Model model1;
         this.playersConnection = new HashMap<>();
 
-        if (playersConnection.containsKey("player0") && playersConnection.containsKey("player1") && playersConnection.containsKey("player2")) {
-            InputStream inputStream = getClass().getResourceAsStream("/config/model_game_almost_won.json");
-            this.model = new ModelBuilder().buildModel(inputStream);
+        Path path = Paths.get("./saves/" + String.join("_", playersConnection.keySet().stream().sorted().toList()) + ".json");
+        if (Files.exists(path)) {
+            System.out.println("From savings");
+            try {
+                FileInputStream fileInputStream = new FileInputStream(path.toFile());
+                model1 = new ModelBuilder().buildModel(fileInputStream);
+                fileInputStream.close();
+            } catch (FileNotFoundException e) {
+                model1 = new ModelBuilder().buildModel(playersConnection.keySet().stream().toList(), false, completeRule);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         } else {
-            this.model = new ModelBuilder().buildModel(playersConnection.keySet().stream().toList(), false, completeRule);
+            System.out.println("Build");
+            model1 = new ModelBuilder().buildModel(playersConnection.keySet().stream().toList(), false, completeRule);
         }
+
+        this.model = model1;
         this.controller = model.getController();
-        for (Player player : controller.getPlayersToPlay()) {
+
+        for (Player player : model.getPlayers()) {
             playersConnection.get(player.getNickname()).send(new AnswerEvent("set_id", player.getId()));
             this.playersConnection.put(player.getId(), playersConnection.get(player.getNickname()));
         }
-    }
 
+        this.saveName = String.join("_", model.getPlayers().stream().map(Player::getNickname).sorted().toList()) + ".json";
+        this.gson = new GsonBuilder()
+                .registerTypeAdapter(Handler.class, new HandlerSerializer())
+                .registerTypeAdapter(Handler.class, new HandlerDeserializer())
+                .create();
+    }
 
     @Override
     public void run() {
@@ -57,6 +81,14 @@ public class GameHandler implements Runnable {
 
     public synchronized void launchAnswerEventEveryone(AnswerEvent answerEvent) {
         playersConnection.values().forEach(connectionHandler -> connectionHandler.send(answerEvent));
+        if (answerEvent.getPropertyName().equals("winner")) {
+            System.out.println("deleting...");
+            try {
+                Files.delete(Paths.get("./saves/" + saveName));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public void launchAnswerEventCurrentPlayer(AnswerEvent answerEvent) {
@@ -67,6 +99,17 @@ public class GameHandler implements Runnable {
                 playersConnection.get(id).send(answerEvent);
             } else {
                 playersConnection.get(id).send(waitEvent);
+            }
+        }
+
+        if (answerEvent.getPropertyName().equals("options")) {
+            try {
+                FileWriter saveFile = new FileWriter("./saves/" + saveName);
+                saveFile.write(this.gson.toJson(this.model));
+                saveFile.close();
+            } catch (IOException e) {
+                System.out.println("Error in saving the game!");
+                //e.printStackTrace();
             }
         }
     }
