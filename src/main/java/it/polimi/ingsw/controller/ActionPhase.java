d440d6be6ce67bee26d7e114e255690ec4ad14b1
AlessandroSironi <alessandro1.sironi@mail.polimi.it>
2022-06-27 17:27:33 +0200

    Controller JavaDoc

--- src/main/java/it/polimi/ingsw/controller/ActionPhase.java
+++ src/main/java/it/polimi/ingsw/controller/ActionPhase.java
@@ -11,15 +11,30 @@ import java.util.List;
 import java.util.Map;
 
 
-//Class ActionPhase keep track of action for the player
+/**
+ * Handling of ActionPhase of the turn of the player
+ */
 
 public class ActionPhase {
-
+    /**
+     * Current Model, state of the game
+     */
     private transient Model model;
+    /**
+     * Current Player's Action Phase handling
+     */
     private transient Player currentPlayer;
+    /**
+     * Map containing methods that can be called and played
+     */
     private final Map<String, Integer> callableMethod;
 
-
+    /**
+     * ActionPhase Constructor, initializes callableMethod Map
+     * @param currentPlayer Player whose turn it is
+     * @param numOfStudentToMove Number of Students to move
+     * @param model Current Model
+     */
     public ActionPhase(Player currentPlayer, int numOfStudentToMove, Model model) {
         this.currentPlayer = currentPlayer;
         this.model = model;
@@ -37,6 +52,12 @@ public class ActionPhase {
         else this.callableMethod.put("playCharacterCard", 0);
     }
 
+    /**
+     * Method for playing a character card in this turn
+     * @param choice CharacterCard to Play
+     * @throws NotEnoughCoinsException if the player doesn't have enough coins
+     * @throws CardException Exception caused by an invalid action upon playing the card
+     */
     public void playCharacterCard(int choice) throws NotEnoughCoinsException, CardException {
         if (callableMethod.get("playCharacterCard") > 0) {
             CharacterCard card = this.model.playCharacterCard(this.currentPlayer.getId(), choice);
@@ -49,6 +70,11 @@ public class ActionPhase {
         }
     }
 
+    /**
+     * Moves a student from the entrance to the player's Dining Room
+     * @param choice student to move
+     * @throws InvalidActionException if action is not possible.
+     */
     public void moveStudentToDiningRoom(int choice) throws InvalidActionException {
         if(callableMethod.get("moveStudentToDiningRoom") > 0) {
             this.model.moveStudentToDiningRoom(this.currentPlayer.getId(), choice);
@@ -56,6 +82,12 @@ public class ActionPhase {
         }
     }
 
+    /**
+     * Moves a student from the entrance to an island.
+     * @param studentChoice student to move
+     * @param islandChoice island destination of the pawn
+     * @throws InvalidActionException if action is not possible
+     */
     public void moveStudentToIsland(int studentChoice, int islandChoice) throws InvalidActionException {
         if(callableMethod.get("moveStudentToIsland") > 0) {
             this.model.moveStudentToIsland(this.currentPlayer.getId(), studentChoice, islandChoice);
@@ -63,6 +95,11 @@ public class ActionPhase {
         }
     }
 
+    /**
+     * Moves MotherNature in the islands
+     * @param stepsChoice steps to move Mother Nature
+     * @throws MotherNatureStepsException if Mother Nature cannot move as requested
+     */
     public void moveMotherNature(int stepsChoice) throws MotherNatureStepsException {
         if(callableMethod.get("moveMotherNature") > 0) {
             this.model.moveMotherNature(this.currentPlayer.getId(), stepsChoice);
@@ -74,6 +111,11 @@ public class ActionPhase {
         }
     }
 
+    /**
+     * Get all students from the selected cloud, and put the students in the player's entrance
+     * @param choice cloud ID to take the student from
+     * @throws CloudException
+     */
     public void takeStudentsFromCloud(int choice) throws CloudException {
         if(callableMethod.get("takeStudentsFromCloud") > 0) {
             this.model.takeStudentsFromCloud(this.currentPlayer.getId(), choice);
@@ -84,6 +126,12 @@ public class ActionPhase {
         }
     }
 
+    /**
+     * Extra action helper method when playing a character card: if the played character card requires an additional move,
+     * this method is invoked
+     * @param values options varying based on the action type
+     * @throws Exception if action is invalid
+     */
     public void extraAction(int ... values) throws Exception {
         if(callableMethod.get("extraAction") > 0) {
             this.model.extraAction(this.currentPlayer.getId(), values);
@@ -91,12 +139,19 @@ public class ActionPhase {
         }
     }
 
+    /**
+     * Ends this ActionPhase for the currentPlayer
+     */
     public void endAction(){
         if(callableMethod.get("endAction") > 0) {
             callableMethod.replaceAll((s, v) -> 0);
         }
     }
 
+    /**
+     * Answers if there are additional actions in this ActionPhase that can be played
+     * @return true if no more actions are possible in this ActionPhase
+     */
     public boolean isEnded() {
       for (String s : callableMethod.keySet()) {
           if (callableMethod.get(s) > 0) return false;
@@ -105,6 +160,9 @@ public class ActionPhase {
       return true;
     }
 
+    /**
+     * Keeps count of the possible moves of the students in this ActionPhase, depending on the number of players in the model
+     */
     private void moveStudentCounter() {
         callableMethod.put("moveStudentToIsland", callableMethod.get("moveStudentToIsland") - 1);
         callableMethod.put("moveStudentToDiningRoom", callableMethod.get("moveStudentToDiningRoom") - 1);
@@ -113,6 +171,9 @@ public class ActionPhase {
         }
     }
 
+    /**
+     * @return Options and actions that can be still be played
+     */
     public List<String> getOptions() {
         List<String> options = new ArrayList<>(callableMethod.entrySet().stream()
                                                         .filter(x -> x.getValue() > 0)
@@ -124,6 +185,11 @@ public class ActionPhase {
         return options;
     }
 
+    /**
+     * Setter method for model
+     * @param model current Model object
+     * @param currentPlayer current player object
+     */
     protected void setModel(Model model, Player currentPlayer) {
         this.model = model;
         this.currentPlayer = currentPlayer;
--- src/main/java/it/polimi/ingsw/controller/PlanningPhase.java
+++ src/main/java/it/polimi/ingsw/controller/PlanningPhase.java
@@ -10,12 +10,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Handling of the Planning Phase in the turn of the player
+ */
 public class PlanningPhase {
-
+    /**
+     * Current model, state of the game
+     */
     private transient Model model;
+    /**
+     * Player currently playing.
+     */
     private transient Player currentPlayer;
+    /**
+     * Map containing methods that can be called and played.
+     */
     private final Map<String, Integer> callableMethod;
 
+    /**
+     * PlanningPhase constructor, initializes callableMethod to get the player to choose the Assistant Card
+     * @param currentPlayer
+     * @param model
+     */
     public PlanningPhase (Player currentPlayer, Model model) {
         this.model = model;
         this.currentPlayer = currentPlayer;
@@ -24,6 +40,11 @@ public class PlanningPhase {
         }};
     }
 
+    /**
+     * Plays the chosen Assistant Card for the currentPlayer
+     * @param choice chosen Assistant Card ID
+     * @throws CardException if the card isn't playable or valid, or if the player cannot play any assistant card
+     */
     public synchronized void playAssistantCard(int choice) throws CardException {
         if (callableMethod.get("playAssistantCard") > 0 && cardIsPlayable(choice)) {
             model.playAssistantCard(this.currentPlayer.getId(), choice);
@@ -34,6 +55,9 @@ public class PlanningPhase {
 
     }
 
+    /**
+     * @return this player's played assistant card
+     */
     private List<Integer> playedAssistantCard() {
         List<Integer> temp = new ArrayList<>();
         for(Player player : this.model.getController().getPlayersHavePlayed()) {
@@ -42,6 +66,9 @@ public class PlanningPhase {
         return temp;
     }
 
+    /**
+     * @return true if exists a card that has not yet been played
+     */
     private boolean existsNotPlayedCard() {
         List<Integer> temp = currentPlayer.getAssistantCards().stream().map(AssistantCard::getValue).toList();
         for (Integer i : temp) {
@@ -50,10 +77,18 @@ public class PlanningPhase {
         return false;
     }
 
+    /**
+     * Answers if an Assistant Card is playable
+     * @param choice AssistantCard to check
+     * @return true if the card has not been yet played or if there are still playable cards available
+     */
     private boolean cardIsPlayable(int choice) {
         return !playedAssistantCard().contains(choice) || !existsNotPlayedCard();
     }
 
+    /**
+     * @return true if the currentPlayer's Planning Phase is ended
+     */
     public boolean isEnded() {
         for (String s : callableMethod.keySet()) {
             if (callableMethod.get(s) > 0) return false;
@@ -61,12 +96,20 @@ public class PlanningPhase {
         return true;
     }
 
+    /**
+     * @return list of options of playable actions
+     */
     public List<String> getOptions() {
         return new ArrayList<>(callableMethod.entrySet().stream()
                 .filter(x -> x.getValue() > 0)
                 .map(Map.Entry::getKey).toList());
     }
 
+    /**
+     * Setter method for the model and currentPlayer
+     * @param model model to set this.model to
+     * @param currentPlayer currentPlayer to set this.currentPlayer to
+     */
     protected void setModel(Model model, Player currentPlayer) {
         this.model = model;
         this.currentPlayer = currentPlayer;
