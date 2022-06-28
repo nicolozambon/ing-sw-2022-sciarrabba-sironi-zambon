d553b58671ad59d184cff3275405c718db50ca79
Jonathan Sciarrabba <jonathan.sciarrabba@mail.polimi.it>
2022-06-28 09:40:39 +0200

    Enums javadoc

--- src/main/java/it/polimi/ingsw/enums/Color.java
+++ src/main/java/it/polimi/ingsw/enums/Color.java
@@ -1,9 +1,32 @@
 package it.polimi.ingsw.enums;
 
+/**
+ * Color of Student and Professor
+ */
 public enum Color {
+
+    /**
+     * Yellow color
+     */
     YELLOW,
+
+    /**
+     * Green color
+     */
     GREEN,
+
+    /**
+     * Blue color
+     */
     BLUE,
+
+    /**
+     * Red color
+     */
     RED,
+
+    /**
+     * Pink color
+     */
     PINK
 }
